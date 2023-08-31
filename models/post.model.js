410886f0564fa3c14da1e6080f309e53a70c6f8a
Rakesh Sojitra <rakesh.sojitra@viitor.cloud>
2023-08-31 19:14:48 +0530

    feat: joi validation integration

--- models/post.model.js
+++ models/post.model.js
@@ -1,13 +1,30 @@
 const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    content: {
-        type: String,
-        required: true
-    }
+  title: {
+    type: String,
+    required: true
+  },
+  author: {
+    type: String,
+    required: true
+  },
+  publicationDate: {
+    type: Date,
+    default: Date.now
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  tags: {
+    type: [String],
+    default: []
+  },
+  status: {
+    type: String,
+    enum: ['draft', 'published', 'archived'],
+    default: 'draft'
+  }
 });
 
 const Post = mongoose.model("Post", PostSchema);
--- routes/post.router.js
+++ routes/post.router.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const post = require('../controllers/post.controller');
- 
-router.post('/', post.add);
+const { validate } = require('../middlewares/post.middleware');
+
+router.post('/', validate, post.add);
 router.get('/', post.get);
 router.get('/:id', post.getById);
 router.delete('/:id', post.delete);
-router.patch('/:id', post.update);
+router.patch('/:id', validate, post.update);
 
 module.exports = router;
